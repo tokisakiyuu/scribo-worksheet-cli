339b5e249842d30e437e22935546f46c888ca34b
tokisakiyuu <tokisakiyuu@qq.com>
2024-02-12 00:29:34 +0800

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,36 @@
-import { useState, useEffect } from 'react'
-import { Text } from 'ink'
+import { Box, Text, useApp, useInput, useStdin } from 'ink'
+import { useState } from 'react'
+import reactUse from 'react-use'
+const { useMount } = reactUse
 
 const App = () => {
-  const [counter, setCounter] = useState(0)
+  const { exit } = useApp()
+  const { isRawModeSupported, setRawMode } = useStdin()
+  const [count, setCount] = useState(0)
 
-  useEffect(() => {
-    const timer = setInterval(() => {
-      setCounter(previousCounter => previousCounter + 1)
-    }, 100)
-
-    return () => {
-      clearInterval(timer)
+  useMount(() => {
+    if (isRawModeSupported) {
+      setRawMode(true)
     }
-  }, [])
+  })
 
-  return <Text color="green">{counter} tests passed</Text>
+  useInput((input, key) => {
+    if (input === 'q') {
+      exit()
+    }
+    if (key.upArrow) {
+      setCount(count + 1)
+    }
+    if (key.downArrow) {
+      setCount(count - 1)
+    }
+  })
+
+  return (
+    <Box>
+      <Text color="green">count: {count}</Text>
+    </Box>
+  )
 }
 
 export default App
--- src/cli.tsx
+++ src/cli.tsx
@@ -1,4 +1,17 @@
 import { render } from 'ink'
+import terminalKit from 'terminal-kit'
 import App from './App.js'
 
-render(<App />)
+const { realTerminal } = terminalKit
+
+// enter alternate screen
+realTerminal.fullscreen(true)
+
+const app = render(<App />, {
+  exitOnCtrlC: false,
+})
+
+app.waitUntilExit().then(() => {
+  // exit alternate screen
+  realTerminal.fullscreen(false)
+})

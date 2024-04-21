46f0aa2e435f6be15dfbcd4de2678121aba021ed
tokisakiyuu <tokisakiyuu@qq.com>
2024-04-21 16:18:46 +0800

    fix

--- src/utils.ts
+++ src/utils.ts
@@ -35,7 +35,7 @@ export function checkoutFromRemoteBranch(branchName: string) {
   if (localBranches.includes(branchName)) {
     execSync(`git checkout ${branchName}`)
   } else {
-    execSync(`git fetch origin`)
+    execSync(`git fetch`)
     execSync(`git checkout -b ${branchName} origin/${branchName}`)
   }
   execSync('git pull')
